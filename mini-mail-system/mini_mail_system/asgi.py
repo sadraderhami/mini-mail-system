16e8a17078568d8f9db57d0664029445aea9b8cc
Sadra <sadraderhami@gmail.com>
2026-08-22 14:08:19 +0330

    Rename and restructure Django project

--- mini-mail-system/mini_mail_system/asgi.py
+++ mini-mail-system/mini_mail_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for myDjangoProject project.
+ASGI config for mini_mail_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myDjangoProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini_mail_system.settings')
 
 application = get_asgi_application()
--- mini-mail-system/mini_mail_system/wsgi.py
+++ mini-mail-system/mini_mail_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myDjangoProject project.
+WSGI config for mini_mail_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myDjangoProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini_mail_system.settings')
 
 application = get_wsgi_application()
